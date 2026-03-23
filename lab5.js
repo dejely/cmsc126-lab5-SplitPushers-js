c4a8bd1d559d73fb83771b699e373e2610eead0f
Dejel Cyrus De Asis <38832544+dejely@users.noreply.github.com>
2026-03-23 13:05:57 +0800

    chore: setup code skeleton

---
+++ lab5.js
@@ -0,0 +1,21 @@
+const available_courses = [
+    "BA Food Appreciation",
+    "BA Applied Poetry of the Future",
+    "BS Computer Repair Shop",
+    "BS Video Gaming",
+    "BS Installing and Downloading",
+]
+
+class Student{
+    
+}
+
+function time_now(){
+    const timeNow = date
+}
+
+const student = []
+
+function add_student(){
+
+}
